54e0e91f6bd7fa9d38273f1753663e015ddcd0b6
tejshahi <tejshahi1984@yahoo.com>
2017-10-26 20:27:12 +0545

    The main text classifier module

--- TextClassifier.py
+++ TextClassifier.py
@@ -1,86 +1,80 @@
 #import sklearn
 import nltk
 import scipy as sp
-from sklearn.datasets import load_mlcomp
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
-
 #CorpusDirTrain = r'E:\Datasets\16NepaliNews\16NepaliNews\Train'
 #CorpusDirTest = r'E:\Datasets\16NepaliNews\16NepaliNews\Test'
 
-CorpusDir=r'E:\Datasets\16NepaliNews\16NepaliNews'
-#trainNews=load_files(CorpusDirTrain, shufffle=True)
-#trainNews=load_files(CorpusDirTest, shufffle=True)
-
-trainNews = load_mlcomp('16NepaliNews', 'train', mlcomp_root= CorpusDir)
-testNews = load_mlcomp('16NepaliNews', 'test', mlcomp_root= CorpusDir)
+CorpusDir = r'E:\Datasets\16NepaliNews\16NepaliNews\16719\raw'
+raw=load_files(CorpusDir, description=None, 
+               load_content=True,
+               encoding='utf-8',
+               decode_error='ignore')
 
 ''' Nepali Stop Words '''
 # The stop words file is copied into the stopwords directory of nltk.data\corpora folder
 
-stopWords = set(nltk.corpus.stopwords.words('nepali'))
+stopWords = set(nltk.corpus.stopwords.words('nepali')) 
 
 
 ''' Testing and Training Data '''
-xTrain = trainNews.data
-xTest = testNews.data
-yTrain = trainNews.target
-yTest = testNews.target
-
+xTrain, xTest, yTrain, yTest=train_test_split(raw.data,
+                                               raw.target,
+                                               test_size=0.1,
+                                               random_state=42)
 ''' feature vector construction '''
-'''tokenizing text with count'''
+''' Vectorizer '''
 
-count_vect = CountVectorizer(tokenizer= lambda x: x.split(" "),
-                                encoding='utf-8',
-                                  decode_error='ignore', analyzer='word',
+tfidfVectorizer = TfidfVectorizer(tokenizer= lambda x: x.split(" "),
+                                  sublinear_tf=True, encoding='utf-8',
+                                  decode_error='ignore',
                                   max_df=0.5,
                                   min_df=10,
                                   stop_words=stopWords)
-X_train_counts = count_vect.fit_transform(xTrain)
-print(X_train_counts.shape)
 
-'''TF-IDF calculation'''
-tf_transformer = TfidfTransformer()
-X_train_tfidf = tf_transformer.fit_transform(X_train_counts)
-print(X_train_tfidf.shape)
+vectorised = tfidfVectorizer.fit_transform(xTrain)
+print('No of Samples , No. of Features ', vectorised.shape)
+''' Classifier '''
 
-#multinomial naive Bayes
+# Multinomial Naive Bayes
 clf1 = Pipeline([
-    ('vect', CountVectorizer),
-    ('tfidf',TfidfTransformer)
-    ('clf', MultinomialNB())
+    ('vect', tfidfVectorizer),
+    ('clf', MultinomialNB(alpha=0.01, fit_prior=True))
 ])
 
-# Linear SVM
+# SVM Linear Kernel
 clf2 = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf',TfidfTransformer())
+    ('vect', tfidfVectorizer),
     ('clf', SVC(kernel='linear'))
 ])
-
-# RBF Kernel SVM
+# SVM RBF Kernel
 clf3 = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf',TfidfTransformer())
+    ('vect', tfidfVectorizer),
     ('clf', SVC(kernel='rbf'))
 ])
 
-# MLP Neural Network
-clf4 = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf',TfidfTransformer())
-    ('clf', MLPClassifier(solver='lbfgs', alpha=1e-5,
-                          hidden_layer_sizes=(5, 2),
-                          random_state=1))
+#MLP Neural Network
+clf4=Pipeline([
+        ('vect', tfidfVectorizer),
+         ('clf', MLPClassifier())
 ])
-#Radial Bias Neural Network
-#need to write, I couldnt fint it on Sklearn
+
+#RBF Neural Network
+#MLP Neural Network
+clf4=Pipeline([
+        ('vect', tfidfVectorizer),
+         ('clf', MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    hidden_layer_sizes=(5, 2), random_state=1))
+])
+
 
 def trainAndEvaluate(clf, xTrain, xTest, yTrain, yTest):
     clf.fit(xTrain, yTrain)
@@ -88,17 +82,19 @@ def trainAndEvaluate(clf, xTrain, xTest, yTrain, yTest):
     print(clf.score(xTrain, yTrain))
     print("Accuracy on Testing Set : ")
     print(clf.score(xTest, yTest))
-    yPred=clf.predict(xTest)
+    yPred = clf.predict(xTest)
+    
     print("Classification Report : ")
     print(metrics.classification_report(yTest, yPred))
     print("Confusion Matrix : ")
     print(metrics.confusion_matrix(yTest, yPred))
-    
+
+
 print('Multinominal Naive Bayes \n')
 trainAndEvaluate(clf1, xTrain, xTest, yTrain, yTest)
-print('Linear Kernel SVC \n')
+print('Linear SVM \n')
 trainAndEvaluate(clf2, xTrain, xTest, yTrain, yTest)
-print('RBF Kernel SVC \n')
+print('RBF SVM \n')
 trainAndEvaluate(clf3, xTrain, xTest, yTrain, yTest)
-print("MLP Neuaral Network")
+print('MLP Neural Network n')
 trainAndEvaluate(clf4, xTrain, xTest, yTrain, yTest)
